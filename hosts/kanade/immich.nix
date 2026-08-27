b7264260d347357f03ddad64fa049b122cf7fb5e
motya <mmotyaa@proton.me>
2026-08-27 15:50:56 +0500

    fix immich permissions

--- hosts/kanade/immich.nix
+++ hosts/kanade/immich.nix
@@ -1,10 +1,21 @@
-{...}:
+{ lib, ...}:
 {
   services.immich = {
     enable = true;
     host = "0.0.0.0";
     port = 2386;
-    mediaLocation = "/home/media/immich";
     openFirewall = true;
+
   };
+
+  systemd.services.immich-server.serviceConfig = {
+    ProtectHome = lib.mkForce "tmpfs";
+    BindPaths = [ "/home/media" ];
+  };
+
+  systemd.services.immich-machine-learning.serviceConfig = {
+    ProtectHome = lib.mkForce "tmpfs";
+    BindPaths = [ "/home/media" ];
+  };
+
 }

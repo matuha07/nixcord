3322c27bd882fc52900065d99d1dc21e0c70d61a
motya <mmotyaa@proton.me>
2026-08-29 15:31:07 +0500

    add agenix secrets for caddy

--- hosts/kanade/caddy.nix
+++ hosts/kanade/caddy.nix
@@ -1,11 +1,20 @@
-{ lib, ... }:
+{ lib, pkgs, config, ... }:
 {
-  services.caddy = {
-    enable = true;
-    virtualHosts."immich.kanade.tail25bcd4.ts.net".extraConfig = "
-      reverse_proxy http://127.0.0.1:2386
-    ";
-  };
+  age.secrets.cloudflare-token.file = ../../secrets/secret1.age;
 
-  systemd.services.caddy.serviceConfig.User = lib.mkForce "root";
+  services.caddy = {
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-a875256c4a171d14cba5d8f727a9ad14ad42621997ce25565a9bc162d6cd8dfa";
+    };
+    enable = true;
+    environmentFile = config.age.secrets.cloudflare-token.path;
+
+    virtualHosts."immich.dogshit.engineer".extraConfig = ''
+      tls {
+        dns cloudflare {env.CF_API_TOKEN}
+      }
+      reverse_proxy http://127.0.0.1:2386
+    '';
+  };
 }
---
+++ secrets/secrets.nix
@@ -0,0 +1,16 @@
+let
+  motyaEnanan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9UcVPDl+dOU+j1yzycCzsUrEPjJ+QxY6ntIL91UecH";
+  motyaAmia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwrLTumQjBG0paEu9Gv9EZGkAbUcPoUWtzliI1xmPto";
+  users = [ motyaEnanan motyaAmia ];
+
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYyzDsbTnA+OhtbSL+vG4Dhg3AUTLP4jZJdFHbYrA8A";
+
+  systems = [system];
+in
+{
+  "secret1.age".publicKeys = [ motyaEnanan system];
+  "armored-secret.age" = {
+    publicKeys = [motyaEnanan];
+    armor = true;
+  };
+}
